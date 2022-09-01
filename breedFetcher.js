98d94150c23099c88077e2c453139ae323912c29
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-01 16:15:04 -0600

    testing and refactoring finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,9 +2,17 @@ const request = require("request");
 
 const fetchBreedDescription = function(breedName, callback) {
   const breedLink = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
   request(breedLink, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
     const data = JSON.parse(body);
-    callback(error, data[0].description);
+    if (!data[0]) {
+      callback("Breed Not Found");
+    } else {
+      callback(null, data[0].description);
+    }
   });
 };
 module.exports = { fetchBreedDescription };
