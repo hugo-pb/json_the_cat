f0f5f8a95a38980989307f41f66630b3f183b1a7
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-01 15:23:22 -0600

    log description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,10 @@
 const request = require("request");
-
-const breedLink = `https://api.thecatapi.com/v1/breeds/search?q=sib`;
+const search = process.argv[2];
+const breedLink = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
 request(breedLink, (error, response, body) => {
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
+  if (error) {
+    console.log("error:", error);
+  }
+  const data = JSON.parse(body);
+  console.log(data[0].description);
 });
